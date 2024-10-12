385b8e5ffdaea4f6f5cce2aec3fcb8a8c88800bb
Ac16ll04ig4 <ameriecallegari@hotmail.com>
2024-10-12 10:43:01 +0200

    oefening 1 en 2

--- startcode/a_favoriete_dieren.py
+++ startcode/a_favoriete_dieren.py
@@ -1,5 +1,8 @@
-# Maak een programma dat:
-# - een lege lijst aanmaakt
-# - drie dieren vraagt aan de user
-# - deze dieren toevoegt aan de lijst
-# - de lijst met dieren toont aan de user
+lijst= []
+input_dier = ""
+input_dier = input("Kies je 3 favoriete dieren, 1 per keer:")
+for i in range(3):
+    input_dier = input("dier " + str(i+1) + ":")
+lijst.append(input_dier)
+print(lijst)
+
--- startcode/b_vind_grootste_getal.py
+++ startcode/b_vind_grootste_getal.py
@@ -1,3 +1,10 @@
-# Maak een lijst met getallen.
+mijn_lijst = [1,2,3,4,5,6,7,8,9,10]
+def vind_grootste_getal(mijn_lijst):
+    grootste_getal = 1
+    for item in mijn_lijst:
+        if item > grootste_getal:
+            grootste_getal = item
+    print(grootste_getal)
 # Schrijf een functie vind_grootste_getal die de grootste waarde uit een lijst teruggeeft.
 # Gebruik een for-loop om de grootste waarde te vinden.
+vind_grootste_getal(mijn_lijst)
